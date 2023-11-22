380a836aa2447682b882fb8ed568b2313f6b46dd
WetBeanProduction <80754269+WetBeanProduction@users.noreply.github.com>
2023-11-22 22:24:53 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace fetchDotnet;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var program = new Program();
+        await program.GetData();
+    }
+
+    public async Task GetData(){
+        var client = new HttpClient();
+
+        try{
+            var response = await client.GetAsync("https://www.localhost:8080/api/");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var data = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
+
+            foreach(var item in data){
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+        catch(HttpRequestException e){
+            Console.WriteLine($"\nException Caught!");
+            Console.WriteLine($"Message {0}", e.Message);
+        }
+    }
+}
